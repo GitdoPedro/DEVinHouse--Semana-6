8cb9927394acead3586d57f1fcea44d0d9133f78
Pedro Lima <emailpedrolima@gmail.com>
2022-11-14 15:08:57 -0300

    exercicio 7

---
+++ Ex07.js
@@ -0,0 +1,11 @@
+const buscaMediaIdade = async (nome) => {
+    try {
+      const resultado = await fetch(`https://api.agify.io/?country_id=BR&name=${nome}`)
+      const data = await resultado.json()
+      console.log(`${nome} - ${data.age}`)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  buscaMediaIdade('Pedro')
